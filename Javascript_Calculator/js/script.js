d4c0f76e23aa36cfb385f9c0f00a213bf1160c23
rakhil <rakhilpr96@gmail.com>
2020-08-18 00:45:42 +0530

    Javascript Calculator

--- Javascript_Calculator/js/script.js
+++ Javascript_Calculator/js/script.js
@@ -1,23 +1,32 @@
 document.getElementById("upper-cont").textContent = 0;
 function Getnum(x){
+	x_num = Number(x);
     var y = document.getElementById("upper-cont").textContent;
     if (y == 0){
     	document.getElementById("upper-cont").textContent = x;
-    }
+	}
+	else if(y == pre_val[0] && x_num >= 0){
+		document.getElementById("upper-cont").textContent = x;
+	}
     else { 
     	document.getElementById("upper-cont").textContent = y + x;
 	}
+	pre_val.splice(0,pre_val.length);
 }
 
+pre_val = [];
 function Result(){
     var res = document.getElementById("upper-cont").textContent;
     res = res.replace("×", "*");
     res = res.replace("÷", "/");
-    document.getElementById("upper-cont").textContent = eval(res);
+	document.getElementById("upper-cont").textContent = eval(res);
+	old_res = document.getElementById("upper-cont").textContent;
+	pre_val.push(old_res);
 }
 
 function clearall() {
 	document.getElementById("upper-cont").textContent = 0;
+	pre_val.splice(0,pre_val.length);
 }
 
 function del() {
@@ -29,6 +38,7 @@ function del() {
 	else{
 	document.getElementById("upper-cont").textContent = d;
 	}
+	pre_val.splice(0,pre_val.length);
 }
 
 function plus_or_minus() {
